4c14ef2f13dc4c96b3172002a5792814efb3a908
Your Name <you@example.com>
2018-08-09 10:40:58 +0300

    changes

--- mpesa/admin.py
+++ mpesa/admin.py
@@ -1,6 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from django.contrib import 
+from rangefilter.filter import DateRangeFilter
+from mpesa_test.mpesa.models import  C2BRequest, OnlineCheckout, OnlineCheckoutResponse
+
 
 # Register your models here.
+@admin.register(C2BRequest)
+class C2BRequestAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'amount', 'name', 'transaction_id', 'transaction_date')
+    readonly_fields = ('phone', 'amount', 'name', 'transaction_id',
+                       'transaction_date', 'business_short_code', 'bill_ref_number', 'invoice_number',
+                       'org_account_balance', 'third_party_trans_id', 'is_validated',
+                       'is_completed', 'date_added')
+    search_fields = ('phone', 'transaction_id', 'name')
+    list_filter = (
+        ('transaction_date', DateRangeFilter),
+        ('date_added', DateRangeFilter)
+    )
+
+
+@admin.register(OnlineCheckout)
+class OnlineCheckoutAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'amount', 'date_added')
+    readonly_fields = ('phone', 'amount', 'checkout_request_id', 'account_reference',
+                       'transaction_description', 'customer_message', 'merchant_request_id',
+                       'response_code', 'response_description', 'date_added')
+    search_fields = ('phone', 'amount', 'date_added')
+    list_filter = (
+        ('date_added', DateRangeFilter),
+    )
+
+
+@admin.register(OnlineCheckoutResponse)
+class OnlineCheckoutResponseAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'amount', 'mpesa_receipt_number', 'transaction_date')
+    readonly_fields = ('phone', 'amount', 'transaction_date', 'mpesa_receipt_number',
+                       'result_description', 'result_code', 'checkout_request_id',
+                       'merchant_request_id', 'date_added')
+    search_fields = ('phone', 'amount', 'date_added', 'mpesa_receipt_number')
+    list_filter = (
+        ('transaction_date', DateRangeFilter),
+        ('date_added', DateRangeFilter)
+    )
--- mpesa/models.py
+++ mpesa/models.py
@@ -2,6 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from mpesa_api.util.managers import AuthTokenManager
+
+
+
+
+class AuthToken(models.Model):
+    """Handles AuthTokens"""
+    access_token = models.CharField(max_length=40);
+    type = models.CharField(max_length=3)
+    expires_in = models.BigIntegerField()
+    objects = AuthTokenManager()
+
+    def __str__(self):
+        return self.access_token
+
+    class Meta:
+        db_table ='tbl_access_token'
 
 # Create your models here.
 class C2BRequest(models.Model):
@@ -35,3 +52,53 @@ class C2BRequest(models.Model):
     @property
     def name(self):
         return '{} {} {}'.format(self.first_name, self.middle_name, self.last_name)
+
+
+
+
+
+class OnlineCheckout(models.Model):
+    """
+    Handles Online Checkout
+    """
+    id = models.BigAutoField(primary_key=True)
+    phone = models.BigIntegerField()
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    checkout_request_id = models.CharField(max_length=50, default='')
+    account_reference = models.CharField(max_length=50, default='')
+    transaction_description = models.CharField(max_length=50, blank=True, null=True)
+    customer_message = models.CharField(max_length=100, blank=True, null=True)
+    merchant_request_id = models.CharField(max_length=50, blank=True, null=True)
+    response_code = models.CharField(max_length=5, blank=True, null=True)
+    response_description = models.CharField(max_length=100, blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.phone)
+
+    class Meta:
+        db_table = 'tbl_online_checkout_requests'
+        verbose_name_plural = 'Online Checkout Requests'
+
+
+class OnlineCheckoutResponse(models.Model):
+    """
+    Handles Online Checkout Response
+    """
+    id = models.BigAutoField(primary_key=True)
+    merchant_request_id = models.CharField(max_length=50, blank=True, null=True)
+    checkout_request_id = models.CharField(max_length=50, default='')
+    result_code = models.CharField(max_length=5, blank=True, null=True)
+    result_description = models.CharField(max_length=100, blank=True, null=True)
+    mpesa_receipt_number = models.CharField(max_length=50, blank=True, null=True)
+    transaction_date = models.DateTimeField(blank=True, null=True)
+    phone = models.BigIntegerField(blank=True, null=True)
+    amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.phone)
+
+    class Meta:
+        db_table = 'tbl_online_checkout_responses'
+        verbose_name_plural = 'Online Checkout Responses'
